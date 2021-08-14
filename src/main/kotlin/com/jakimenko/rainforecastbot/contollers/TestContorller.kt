2a5ee8e00f96bdd4e0fd0a34cd4257caa8ae1cea
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-08-14 22:27:37 +0300

    new test method

--- src/main/kotlin/com/jakimenko/rainforecastbot/contollers/TestContorller.kt
+++ src/main/kotlin/com/jakimenko/rainforecastbot/contollers/TestContorller.kt
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestContorller {
 
+    @GetMapping
+    fun mainTest() =
+        BaseResponse(200, "main, this is success")
 
     @GetMapping("/test")
     fun testFunction() =
