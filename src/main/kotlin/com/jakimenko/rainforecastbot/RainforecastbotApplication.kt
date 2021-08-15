04b8a42a053fa894dc8bd92db1a7df697f760808
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-08-15 10:50:48 +0300

    refactoring

--- src/main/kotlin/com/jakimenko/rainforecastbot/RainforecastbotApplication.kt
+++ src/main/kotlin/com/jakimenko/rainforecastbot/RainforecastbotApplication.kt
@@ -4,8 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RainforecastbotApplication
-
-fun main(args: Array<String>) {
-	runApplication<RainforecastbotApplication>(*args)
+class RainforecastbotApplication {
+	fun main(args: Array<String>) {
+		runApplication<RainforecastbotApplication>(*args)
+	}
 }
+
+//fun main(args: Array<String>) {
+//	runApplication<RainforecastbotApplication>(*args)
+//}
