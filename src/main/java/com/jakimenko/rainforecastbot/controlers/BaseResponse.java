cce76db5e583b8212d528df68523de291bb87958
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-08-15 11:07:01 +0300

    refactoring

--- src/main/java/com/jakimenko/rainforecastbot/controlers/BaseResponse.java
+++ src/main/java/com/jakimenko/rainforecastbot/controlers/BaseResponse.java
@@ -1,11 +1,35 @@
 package com.jakimenko.rainforecastbot.controlers;
 
 public class BaseResponse {
-    private final int code;
-    private final String message;
+    private int code;
+    private String message;
 
     public BaseResponse(int code, String message) {
         this.code = code;
         this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseResponse{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
--- src/main/java/com/jakimenko/rainforecastbot/controlers/TestController.java
+++ src/main/java/com/jakimenko/rainforecastbot/controlers/TestController.java
@@ -1,13 +1,20 @@
 package com.jakimenko.rainforecastbot.controlers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class TestController {
 
-    @GetMapping
-    public BaseResponse testMethod() {
+    @GetMapping("/")
+    public BaseResponse zeroTestMethod() {
         return new BaseResponse(200, "main test method");
+    }
+
+    @GetMapping("test")
+    public BaseResponse testMethod() {
+        return new BaseResponse(200, "test method");
     }
 }
