c452924a608947219f0c6cbb43f3802a1694bb30
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-08-17 21:51:15 +0300

    refactoring

--- src/main/kotlin/com/jakimenko/rainforecastbot/controllers/TestController.kt
+++ src/main/kotlin/com/jakimenko/rainforecastbot/controllers/TestController.kt
@@ -1,16 +1,18 @@
 package com.jakimenko.rainforecastbot.controllers
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
 class TestController {
 
     @GetMapping("test")
-    fun testMethod() = "Test main method"
+    fun testMethod() = System.getenv("TGTOKEN")
 
     @GetMapping
     fun rootTestMethod() = "Root test method"
+
+    // http://localhost:8080/double?msg=mytext
+    @GetMapping("double")
+    fun doubbledMsg(@RequestParam("msg") msg: String) = msg+msg
 }
--- src/test/kotlin/com/jakimenko/rainforecastbot/RainforecastbotApplicationTests.kt
+++ src/test/kotlin/com/jakimenko/rainforecastbot/RainforecastbotApplicationTests.kt
@@ -3,11 +3,20 @@ package com.jakimenko.rainforecastbot
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+//@SpringBootTest
 class RainforecastbotApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		println("start")
+		val arr = IntArray(5)
+		arr[1] = 5
+		var i = 0
+		while (i < arr.size) {
+			println("i = ${i}")
+			println("arr[i] = ${arr[i]}")
+			i = i.inc()
+		}
 	}
 
 }
