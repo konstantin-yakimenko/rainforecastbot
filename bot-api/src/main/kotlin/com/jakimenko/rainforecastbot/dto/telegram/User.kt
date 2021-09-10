7bf066431aa7252aacf848490cd875f3e86b84a9
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-09-11 00:15:19 +0300

    refactoring

--- bot-api/src/main/kotlin/com/jakimenko/rainforecastbot/dto/telegram/User.kt
+++ bot-api/src/main/kotlin/com/jakimenko/rainforecastbot/dto/telegram/User.kt
@@ -1,7 +1,12 @@
 package com.jakimenko.rainforecastbot.dto.telegram
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class User(
     var id: Int,
+    @JsonProperty("is_bot")
     var is_bot: Boolean?,
     var first_name: String?,
     var last_name: String?,
