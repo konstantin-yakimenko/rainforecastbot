dffa37d717380d565bca88bf52c5c5ac57dbc080
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-11-19 20:39:01 +0300

    refactoring

--- bot-server/src/main/kotlin/com/jakimenko/rainforecastbot/service/WeatherApiCallImpl.kt
+++ bot-server/src/main/kotlin/com/jakimenko/rainforecastbot/service/WeatherApiCallImpl.kt
@@ -2,11 +2,8 @@ package com.jakimenko.rainforecastbot.service
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jakimenko.rainforecastbot.dto.openweathermap.WeatherInfo
-import kotlinx.coroutines.async
 import mu.KLogging
-import okhttp3.Call
 import okhttp3.Request
-import okhttp3.Response
 
 
 class WeatherApiCallImpl<T: WeatherInfo>: WeatherApiCall<T> {
@@ -15,13 +12,15 @@ class WeatherApiCallImpl<T: WeatherInfo>: WeatherApiCall<T> {
     }
 
     override suspend fun <T: WeatherInfo> callWeatherApi(url: String, clazz: Class<T>, httpClient: HttpClient): T {
-        return scope.async {
-            val request: Request = Request.Builder()
-                .url(url)
-                .build()
-            val call: Call = httpClient.client.newCall(request)
-            val response: Response = call.execute()
-            return@async mapper.readValue(response.body?.bytes()!!, clazz)
-        }.await()
+        val request: Request = Request.Builder()
+            .url(url)
+            .build()
+        return httpClient
+            .client
+            .newCall(request)
+            .execute()
+            .use {
+                mapper.readValue(it.body?.bytes()!!, clazz)
+            }
     }
 }
