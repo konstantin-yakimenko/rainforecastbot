f9ba1e7eba3e0b409300fd628ab6ec00dc269d85
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-11-19 19:51:17 +0300

    refactoring

--- bot-server/src/test/kotlin/com/jakimenko/rainforecast/CallOpenWeatherMap.kt
+++ bot-server/src/test/kotlin/com/jakimenko/rainforecast/CallOpenWeatherMap.kt
@@ -1,21 +1,49 @@
 package com.jakimenko.rainforecast
 
-//import com.jakimenko.rainforecastbot.dto.openweathermap.OnecallResponse
-//import io.ktor.client.*
-//import io.ktor.client.engine.java.*
-//import io.ktor.client.request.*
-//import kotlinx.coroutines.runBlocking
-//import io.ktor.client.call.*
-//import io.ktor.client.statement.*
-//import java.time.Instant
-//import java.time.ZoneId
-//import java.time.ZonedDateTime
-//import java.time.temporal.ChronoUnit
+import com.jakimenko.rainforecastbot.dto.openweathermap.CurrentWeatherInCity
+import com.jakimenko.rainforecastbot.dto.openweathermap.OnecallResponse
+import com.jakimenko.rainforecastbot.service.WeatherApiCallImpl
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import org.junit.jupiter.api.Test
 
 
 class CallOpenWeatherMap {
+
+/*
     @Test
     fun test() {
+        runBlocking {
+
+            println("================ start")
+            val res = async {
+                val client: OkHttpClient = OkHttpClient.Builder()
+                    .build()
+
+                // CurrentWeatherInCity::class.java
+                // OnecallResponse::class.java
+                val clazz = CurrentWeatherInCity::class.java
+//            val clazz = OnecallResponse::class.java
+//            val url = "https://api.openweathermap.org/data/2.5/onecall?lat=55.751442&lon=37.615569&exclude=daily,alerts&units=metric&lang=ru&appid="
+                val url = "https://api.openweathermap.org/data/2.5/weather?lat=55.751442&lon=37.615569&units=metric&lang=ru&appid="
+
+                val request: Request = Request.Builder()
+                    .url(url)
+                    .build()
+                val call: Call = client.newCall(request)
+                val response: Response = call.execute()
+                return@async WeatherApiCallImpl.mapper.readValue(response.body?.bytes()!!, clazz)
+            }.await()
+
+            println("================")
+            println("res = ${res}")
+            println("================ finish")
+        }
     }
+*/
+
 }
